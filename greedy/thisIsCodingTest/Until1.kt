afb27b3dfdf086667ffffd96edfcc5f75e137e34
ddanglehee <shee9812@naver.com>
2022-01-20 00:04:49 +0900

    [Greedy] 이것이 코딩 테스트다 - 1이 될 때까지 더 효율적인 코드로 수정

--- greedy/thisIsCodingTest/Until1.kt
+++ greedy/thisIsCodingTest/Until1.kt
@@ -5,12 +5,18 @@ class Until1 {
         var result = 0
 
         while (n != 1) {
-            if (n % k == 0) {
-                n /= k
+            if (k <= n) {
+                if (n % k == 0) {
+                    n /= k
+                } else {
+                    n -= 1
+                }
+                result++
             } else {
-                n -= 1
+                // n이 k보다 작아질 경우 1씩 n-1번 빼는 시간을 아낄 수 있다.
+                result += n - 1
+                n = 1
             }
-            result++
         }
 
         println(result)
