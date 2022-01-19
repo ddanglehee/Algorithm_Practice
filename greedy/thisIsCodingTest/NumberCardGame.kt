ca64051e006f6a2100f25738e807333ed835024d
ddanglehee <shee9812@naver.com>
2022-01-19 23:25:51 +0900

    [Greedy] 이것이 코딩 테스트다 - 숫자 카드 게임 풀이

---
+++ greedy/thisIsCodingTest/NumberCardGame.kt
@@ -0,0 +1,18 @@
+class NumberCardGame {
+    fun solution() {
+        val (n, m) = readLine()!!.split(" ").map { it.toInt() }
+        val card = mutableListOf<List<Int>>()
+        for (i in 0 until n) {
+            val input = readLine()!!.split(" ").map { it.toInt() }
+            card.add(input)
+        }
+
+        var result = 0
+        for (i in 0 until n) {
+            val min = card[i].minOrNull()!! // O(M)
+            result = maxOf(result, min)
+        } // O(NM)
+
+        println(result)
+    }
+}
