e84e19e7dbb6cad1c1456b7cec16a29946910375
ddanglehee <shee9812@naver.com>
2022-01-25 17:52:48 +0900

    [Greedy] BOJ - 2847번 게임을 만든 동준이 풀이

---
+++ greedy/boj/Boj2847.kt
@@ -0,0 +1,22 @@
+class Boj2847 {
+    fun solution() {
+        val n = readLine()!!.toInt()
+        val scores = mutableListOf<Int>()
+
+        for (i in 0 until n) {
+            val score = readLine()!!.toInt()
+            scores.add(score)
+        }
+
+        var result = 0
+        scores.reverse()
+        for (i in 0 until scores.lastIndex) {
+            if (scores[i] > scores[i + 1]) continue
+
+            result += scores[i + 1] - scores[i] + 1
+            scores[i + 1] = scores[i] - 1
+        }
+
+        println(result)
+    }
+}
