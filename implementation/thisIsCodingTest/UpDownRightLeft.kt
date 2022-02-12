0f4ba5fd4de6a989edfd3b5749c388592abb36dd
ddanglehee <shee9812@naver.com>
2022-02-12 21:48:49 +0900

    [Implementation] 이것이 코딩 테스트다 - 상하좌우 리팩토링

--- implementation/thisIsCodingTest/UpDownRightLeft.kt
+++ implementation/thisIsCodingTest/UpDownRightLeft.kt
@@ -2,36 +2,26 @@ class UpDownRightLeft {
     fun solution() {
         val br = System.`in`.bufferedReader()
         val n = br.readLine().toInt()
-        val input = br.readLine().split(" ")
+        val input = br.readLine().split(" ").map{ it.single() } // CharSequence.single()을 통해 input을 Char리스트로 만듦
 
-        var curRow = 1
-        var curColumn = 1
+        val dx = listOf(0, 0, -1, 1)
+        val dy = listOf(-1, 1, 0, 0)
+        val directions = charArrayOf('L', 'R', 'U', 'D')
+
+        var curX = 1
+        var curY = 1
 
         for (direction in input) {
-            when (direction) {
-                "L" -> {
-                    if (curColumn != 1) {
-                        curColumn--
-                    }
-                }
-                "R" -> {
-                    if (curColumn != n) {
-                        curColumn++
-                    }
-                }
-                "U" -> {
-                    if (curRow != 1) {
-                        curRow--
-                    }
-                }
-                "D" -> {
-                    if (curRow != n) {
-                        curRow++
-                    }
-                }
+            val directionIndex = directions.indexOf(direction)
+            val tmpX= curX + dx[directionIndex]
+            val tmpY = curY + dy[directionIndex]
+
+            if (tmpX in 1..n && tmpY in 1..n) {
+                curX = tmpX
+                curY = tmpY
             }
         }
 
-        println("$curRow $curColumn")
+        println("$curX $curY")
     }
 }
