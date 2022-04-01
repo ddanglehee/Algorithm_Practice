2914c6d3ab333ea96c146f1745577e9c001afee0
ddanglehee <shee9812@naver.com>
2022-04-01 16:30:45 +0900

    [Shortest Path] 이것이 코딩 테스트다 - 다익스트라 알고리즘 기본 코드 리팩토링 (우선순위 큐 사용)

--- shortestpath/DijkstraBasic.kt
+++ shortestpath/DijkstraBasic.kt
@@ -4,7 +4,6 @@ class DijkstraBasic {
         val (n, m) = br.readLine().split(" ").map { it.toInt() } // n: 노드의 개수, m: 간선의 개수
         val start = br.readLine().toInt() // 시작점
         val graph = List(n + 1) { mutableListOf<Pair<Int, Int>>() }
-        val visited = MutableList(n + 1) { false }
         val distance = MutableList(n + 1) { INF }
 
         repeat(m) {
@@ -12,41 +11,30 @@ class DijkstraBasic {
             graph[a].add(b to c)
         }
 
-        dijkstra(start, graph, visited, distance)
+        dijkstra(start, graph, distance)
     }
 
-    // 다익스트라 알고리즘 : O(V^2)
-    fun dijkstra(start: Int, graph: List<MutableList<Pair<Int, Int>>>, visited: MutableList<Boolean>, distance: MutableList<Int>) {
-        distance[start] = 0
-        visited[start] = true
-        graph[start].forEach { adjacentNode ->
-            distance[adjacentNode.first] = adjacentNode.second
-        }
+    // 다익스트라 알고리즘
+    fun dijkstra(start: Int, graph: List<MutableList<Pair<Int, Int>>>, distance: MutableList<Int>) {
+        val priorityQueue = PriorityQueue<Pair<Int, Int>>(compareBy { it.second })
 
-        repeat(graph.size - 1) {
-            val currentNode = getSmallestNode(visited, distance)
-            visited[currentNode] = true
+        distance[start] = 0
+        priorityQueue.offer(start to distance[start])
+
+        while(priorityQueue.isNotEmpty()) {
+            val (currentNode, dist) = priorityQueue.poll()
+
+            if (distance[currentNode] < dist) continue // 현재 노드가 이미 처리된 적이 있는 노드라면 무시 (visited 대신)
+
             graph[currentNode].forEach { adjacentNode ->
                 if (distance[currentNode] + adjacentNode.second < distance[adjacentNode.first]) {
                     distance[adjacentNode.first] = distance[currentNode] + adjacentNode.second
+                    priorityQueue.offer(adjacentNode.first to distance[adjacentNode.second])
                 }
             }
         }
     }
 
-    // 방문하지 않은 노드 중에서 가장 최단 거리가 짧은 노드의 번호 반환
-    private fun getSmallestNode(visited: List<Boolean>, distance: List<Int>): Int {
-        var min = INF
-        var minIndex = -1
-
-        visited.forEachIndexed { index, visited ->
-            if (!visited && distance[index] < min) {
-                min = distance[index]
-                minIndex = index
-            }
-        }
-        return minIndex
-    }
     companion object {
         const val INF = 987654321
     }
