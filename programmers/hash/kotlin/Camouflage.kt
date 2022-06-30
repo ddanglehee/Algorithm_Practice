64c024011fc6513865a3fe87ae60b8ceca43b368
ddanglehee <shee9812@naver.com>
2022-06-30 22:24:20 +0900

    [Hash] 프로그래머스 - 위장 groupBy를 사용해서 풀이

--- programmers/hash/kotlin/Camouflage.kt
+++ programmers/hash/kotlin/Camouflage.kt
@@ -1,18 +1,10 @@
 class Camouflage {
     fun solution(clothes: Array<Array<String>>): Int {
         var answer = 1
-        val clothesMap = HashMap<String, Int>()
+        val clothesMap = clothes.groupBy { it[1] }
 
-        clothes.forEach { c ->
-            if (clothesMap.containsKey(c[1])) {
-                clothesMap[c[1]] = (clothesMap[c[1]]?.plus(1))!!
-            } else {
-                clothesMap.put(c[1], 1)
-            }
-        }
-
-        clothesMap.forEach { (key, _) ->
-            answer *= (clothesMap[key]?.plus(1))!!
+        clothesMap.values.forEach {
+            answer *= (it.size + 1)
         }
         return answer - 1
     }
