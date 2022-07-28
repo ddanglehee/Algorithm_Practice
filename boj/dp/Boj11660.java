c77deee6f33d9d1577877831dd69aba22f714bf2
ddanglehee <shee9812@naver.com>
2022-07-28 22:32:13 +0900

    [Dynamic Programming] BOJ - 11660번 구간 합 구하기5 풀이

---
+++ boj/dp/Boj11660.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+public class Boj11660 {
+
+    static final int MAX = 1025;
+    static int N, M;
+    static int[][] dp = new int[MAX][MAX];
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= N; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            sb.append(dp[x2][y2] - dp[x1 - 1][y2] - dp[x2][y1 - 1] + dp[x1 - 1][y1 - 1]).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
