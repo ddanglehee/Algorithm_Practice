7851cead542ae9a0c19a4f4be25bee3b5b094a03
ddanglehee <shee9812@naver.com>
2022-08-09 12:26:32 +0900

    [Heap] 프로그래머스 - 이중우선순위큐 2번째 풀이 추가

--- programmers/heap/DualPriorityQueue.kt
+++ programmers/heap/DualPriorityQueue.kt
@@ -1,8 +1,10 @@
+import java.util.*
+
 class DualPriorityQueue {
     fun solution(operations: Array<String>): IntArray {
         var answer = intArrayOf(0, 0)
 
-        val queue = ArrayDeque<Int>()
+        val queue = kotlin.collections.ArrayDeque<Int>()
         operations.forEach { operation ->
             val (command, number) = operation.split(" ")
 
@@ -12,20 +14,51 @@ class DualPriorityQueue {
                     queue.sort()
                 }
                 "D" -> {
-                    if (queue.isNotEmpty()) {
-                        if (number.toInt() < 0) {
-                            queue.removeFirstOrNull()
-                        } else {
-                            queue.removeLastOrNull()
-                        }
+                    if (number.toInt() < 0) {
+                        queue.removeFirstOrNull()
+                    } else {
+                        queue.removeLastOrNull()
                     }
                 }
+
             }
         }
 
         if (queue.isNotEmpty()) {
             answer[0] = queue.removeLast()
             answer[1] = queue.removeFirst()
+        }
+
+        return answer
+    }
+
+    fun solution2(operations: Array<String>): IntArray {
+        var answer = intArrayOf(0, 0)
+
+        val maxHeap = PriorityQueue<Int>(Collections.reverseOrder())
+        val minHeap = PriorityQueue<Int>()
+
+        operations.forEach { operation ->
+            val (command, number) = operation.split(" ")
+
+            when (command) {
+                "I" -> {
+                    maxHeap.offer(number.toInt())
+                    minHeap.offer(number.toInt())
+                }
+                "D" -> {
+                    if (number.toInt() > 0) {
+                        maxHeap.poll()?.let { minHeap.remove(it) }
+                    } else {
+                        minHeap.poll()?.let { maxHeap.remove(it) }
+                    }
+                }
+            }
+        }
+
+        if (maxHeap.isNotEmpty()) {
+            answer[0] = maxHeap.peek()
+            answer[1] = minHeap.peek()
         }
 
         return answer
