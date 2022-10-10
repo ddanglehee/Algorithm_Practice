c2840353a19d589c6e482db05e5478f999dc394d
ddanglehee <shee9812@naver.com>
2022-10-10 13:21:38 +0900

    [Implementation] BOJ - 3085번 사탕 게임 풀이

---
+++ boj/implementation/kotlin/Boj3085.kt
@@ -0,0 +1,71 @@
+class Boj3085 {
+
+    fun main() = with (System.`in`.bufferedReader()) {
+        val n = readLine().toInt()
+        val candies = Array(n) { charArrayOf() }
+
+        for (i in 0 until n) {
+            candies[i] = readLine().toCharArray()
+        }
+
+        var result = 0
+
+        val dy = intArrayOf(-1, 0, 1, 0)
+        val dx = intArrayOf(0, 1, 0, -1)
+        for (i in 0 until n) {
+            for (j in 0 until n) {
+                for (d in 0..3) {
+                    val changeY = i +dy[d]
+                    val changeX = j + dx[d]
+
+                    if (changeY in 0 until n && changeX in 0 until n) {
+                        if (candies[i][i] == candies[changeY][changeX]) continue
+                        swap(candies, i, j, changeY, changeX)
+                        result = maxOf(result, countCandy(candies))
+                        swap(candies, changeY, changeX, i, j)
+                    }
+                }
+            }
+        }
+
+        println(result)
+    }
+
+    fun swap(candies: Array<CharArray>, i: Int, j: Int, k: Int, l:Int) {
+        val tmp = candies[i][j]
+        candies[i][j] = candies[k][l]
+        candies[k][l] = tmp
+    }
+
+    fun countCandy(candies: Array<CharArray>): Int {
+        var iMax = 1
+        var jMax = 1
+
+
+        for (i in candies.indices) {
+            var count = 1
+            for (j in 1 until candies.size) {
+                if (candies[i][j] == candies[i][j - 1]) {
+                    count++
+                    iMax = maxOf(iMax, count)
+                } else {
+                    count = 1
+                }
+            }
+        }
+
+        for (j in candies.indices) {
+            var count = 1
+            for (i in 1 until candies.size) {
+                if (candies[i][j] == candies[i - 1][j]) {
+                    count++
+                    iMax = maxOf(iMax, count)
+                } else {
+                    count = 1
+                }
+            }
+        }
+
+        return maxOf(iMax, jMax)
+    }
+}
