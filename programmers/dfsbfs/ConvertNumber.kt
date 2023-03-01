05fe6bf94cfb26ecb027939dbe12e631438bd7c9
ddanglehee <shee9812@naver.com>
2023-03-01 21:01:46 +0900

    [Dynamic Programming] 프로그래머스 - 숫자 변환하기 DP 풀이 추가

--- programmers/dfsbfs/ConvertNumber.kt
+++ programmers/dfsbfs/ConvertNumber.kt
@@ -1,4 +1,5 @@
 class ConvertNumber {
+    // BFS를 이용한 풀이
     fun solution(x: Int, y: Int, n: Int): Int = bfs(x, y, n)
 
     private fun bfs(x: Int, y: Int, n: Int): Int {
@@ -22,5 +23,21 @@ class ConvertNumber {
         }
 
         return -1
+    }
+
+    // DP를 이용한 풀이
+    private solution2(x: Int, y: Int, n: Int): Int {
+        val dpTable = IntArray(y + 1) { 1000001 }
+
+        dpTable[x] = 0
+        for (i in (x + 1)..y) {
+            val a = if (x <= i - n) dpTable[i - n] + 1 else 1000001
+            val b = if (i % 2 == 0) dpTable[i / 2] + 1 else 1000001
+            val c = if (i % 3 == 0) dpTable[i / 3] + 1 else 1000001
+
+            dpTable[i] = minOf(a, minOf(b, c))
+        }
+
+        return if (1000000 < dpTable[y]) -1 else dpTable[y]
     }
 }
