dcf2e6aa921dd153a8b6d93ffb72230ffb80c875
ddanglehee <shee9812@naver.com>
2023-04-13 23:47:29 +0900

    [Bit Masking] BOJ - 11724번 집합 다른 풀이 추가

--- boj/bitmasking/Boj11724.kt
+++ boj/bitmasking/Boj11724.kt
@@ -1,6 +1,5 @@
 class Boj11724 {
 
-
     fun main() = with(System.`in`.bufferedReader()) {
         val sb = StringBuilder()
 
@@ -37,6 +36,48 @@ class Boj11724 {
                     }
                     else -> {
                         S[number.toInt()] = !S[number.toInt()]
+                    }
+                }
+            }
+        }
+
+        print(sb)
+    }
+
+    // 새로운 풀이
+    fun main2() = with(System.`in`.bufferedReader()) {
+        val sb = StringBuilder()
+
+        val m = readLine().toInt()
+        var s = 0
+
+        repeat(m) {
+            val input = readLine()
+
+            if (input == "all") {
+                s = (1 shl 20) - 1
+            } else if (input == "empty") {
+                s = 0
+            } else {
+                val (command, nString) = input.split(" ")
+                val number = nString.toInt() - 1
+
+                when (command) {
+                    "add" -> {
+                        s = s or (1 shl number)
+                    }
+                    "remove" -> {
+                        s = s and (1 shl number).inv()
+                    }
+                    "check" -> {
+                        if (1 <= (s and (1 shl number))) {
+                            sb.append(1).append("\n")
+                        } else {
+                            sb.append(0).append("\n")
+                        }
+                    }
+                    else -> {
+                        s = s xor (1 shl number)
                     }
                 }
             }
