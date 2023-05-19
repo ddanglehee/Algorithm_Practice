5af410146b3f1a66cea409a6ed3446b3cc44f5d8
ddanglehee <shee9812@naver.com>
2023-05-19 22:23:51 +0900

    [BFS/DFS] 프로그래머스 - 경주로 건설 풀이

--- programmers/dfsbfs/ConstructionOfRaceway.kt
+++ programmers/dfsbfs/ConstructionOfRaceway.kt
@@ -7,10 +7,44 @@ class Solution {
     fun solution(board: Array<IntArray>): Int {
         val n = board.size
 
-        dfs(n, board, Array(n) { BooleanArray(n) }, 0, 0, 2, 0)
-        dfs(n, board, Array(n) { BooleanArray(n) }, 0, 0, 3, 0)
+        // dfs(n, board, Array(n) { BooleanArray(n) }, 0, 0, 2, 0)
+        // dfs(n, board, Array(n) { BooleanArray(n) }, 0, 0, 3, 0)
+        bfs(n, board, 0, 0, 0)
+        bfs(n, board, 0, 0, 1)
 
         return answer
+    }
+
+    fun bfs(n: Int, board: Array<IntArray>, r: Int, c: Int, d: Int) {
+        val queue = ArrayDeque<Triple<Pair<Int, Int>, Int, Int>>()
+        queue.add(Triple(Pair(r, c), d, 0))
+        val costMap = Array(n) { IntArray(n) }
+        costMap[r][c] = 1
+
+        while (queue.isNotEmpty()) {
+            val (location, direction, cost) = queue.removeFirst()
+
+            if (location.first == n - 1 && location.second == n - 1) {
+                answer = minOf(answer, cost)
+                continue
+            }
+
+            for (i in 0 until 4) {
+                val nR = location.first + dr[i]
+                val nC = location.second + dc[i]
+
+                if (nR in 0 until n && nC in 0 until n && board[nR][nC] == 0) {
+                    val addition = if (direction == i) 100 else 600
+                    if (costMap[nR][nC] == 0) {
+                        costMap[nR][nC] = cost + addition
+                        queue.add(Triple(Pair(nR, nC), i, cost + addition))
+                    } else if (cost + addition < costMap[nR][nC]) {
+                        costMap[nR][nC] = cost + addition
+                        queue.add(Triple(Pair(nR, nC), i, cost + addition))
+                    }
+                }
+            }
+        }
     }
 
     fun dfs(n: Int, board: Array<IntArray>, visited: Array<BooleanArray>, r: Int, c: Int, d: Int, cost: Int) {
