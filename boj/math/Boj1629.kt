40782b7068d8629fe062a347f62ecb5b49de71bb
ddanglehee <shee9812@naver.com>
2023-06-03 15:31:47 +0900

    [Math] BOJ - 1629번 곱셈 반복문 풀이

--- boj/math/Boj1629.kt
+++ boj/math/Boj1629.kt
@@ -1,5 +1,6 @@
 class Boj1629 {
 
+    // 1. 재귀를 이용한 풀이
     fun main() = with(System.`in`.bufferedReader()) {
         val (a, b, c) = readLine().split(" ").map { it.toLong() }
 
@@ -12,5 +13,25 @@ class Boj1629 {
         val tmp = solution(a, b / 2, c) % c
 
         return if (b % 2 == 0L) tmp * tmp % c else ((tmp * tmp % c) * (a % c)) % c
+    }
+
+    // 2. 반복문을 이용한 풀이
+    fun main() = with(System.`in`.bufferedReader()) {
+        var (a, b, c) = readLine().split(" ").map { it.toLong() }
+
+        var answer = 1L
+
+        while (b != 0L) {
+            if (b % 2L == 1L) {
+                answer *= a
+                answer %= c
+            }
+
+            a *= a
+            a %= c
+            b /= 2L
+        }
+
+        print(answer)
     }
 }
