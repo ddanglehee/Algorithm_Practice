c6e2ef57e1a98e22d3c0c8b2ece55305bec5c7f9
ddanglehee <shee9812@naver.com>
2023-06-06 13:07:10 +0900

    [BFS] 프로그래머스 - 부대복귀 풀이

--- programmers/dfsbfs/UnitReturn.kt
+++ programmers/dfsbfs/UnitReturn.kt
@@ -1,4 +1,44 @@
 class Solution {
+    // 정답 풀이 (destination에서 출발하는 걸 왜 생각 못했을까ㅠ!!)
+    fun solution(n: Int, roads: Array<IntArray>, sources: IntArray, destination: Int): IntArray {
+        val answer = IntArray(sources.size)
+        val graph = Array(n + 1) {
+            mutableListOf<Int>()
+        }
+        val dist = IntArray(n + 1) { -1 }
+
+        roads.forEach { (a, b) ->
+            graph[a].add(b)
+            graph[b].add(a)
+        }
+
+        bfs(destination, graph, dist)
+
+        for (i in sources.indices) {
+            answer[i] = dist[sources[i]]
+        }
+
+        return answer
+    }
+
+    fun bfs(start: Int, graph: Array<MutableList<Int>>, dist: IntArray) {
+        val queue = ArrayDeque<Pair<Int, Int>>()
+        dist[start] = 0
+        queue.add(start to 0)
+
+        while (queue.isNotEmpty()) {
+            val (node, d) = queue.removeFirst()
+
+            graph[node].forEach { next ->
+                if (dist[next] == -1) {
+                    dist[next] = d + 1
+                    queue.add(next to d + 1)
+                }
+            }
+        }
+    }
+
+    // 시간 초과 풀이
     fun solution(n: Int, roads: Array<IntArray>, sources: IntArray, destination: Int): IntArray {
         val answer = IntArray(sources.size)
         val graph = Array(n + 1) {
