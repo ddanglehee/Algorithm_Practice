680b66a2aea9a35fdccb2ea0b8d4b9461a08a407
ddanglehee <shee9812@naver.com>
2023-08-08 11:29:26 +0900

    [Dynamic Programming] BOJ - 1309번 동물원 풀이

---
+++ boj/dp/java/Boj1309.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Boj1309 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] dp = new int[n + 1][2]; // dp[i][0]: 세로 i칸에 사자가 0마리인 경우, dp[i][1]: 세로 i칸에 사자가 1마리인 경우
+        dp[1][0] = 1; dp[1][1] = 2;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i][0] = (dp[i - 1][0] + dp[i - 1][1]) % 9901;
+            dp[i][1] = (dp[i - 1][1] + 2 * dp[i - 1][0]) % 9901;
+        }
+
+        System.out.print((dp[n][0] + dp[n][1]) % 9901);
+    }
+}
