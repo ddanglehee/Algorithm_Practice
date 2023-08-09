27fc0ecfd525395317005d581d09317d6321e9ec
ddanglehee <shee9812@naver.com>
2023-08-09 16:10:44 +0900

    [Dynamic Programming] BOJ - 1309번 동물원 1차원 배열 풀이

--- boj/dp/java/Boj1309.java
+++ boj/dp/java/Boj1309.java
@@ -2,6 +2,22 @@ import java.io.*;
 
 public class Boj1309 {
 
+    // 1차원 배열 풀이
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[n + 1];
+        dp[0] = 1; dp[1] = 3
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = (2 * dp[i - 1] + dp[i - 2])  % 9901;
+        }
+
+        System.out.print(dp[n]) % 9901);
+    }
+
+    // 2차원 배열 풀이
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
