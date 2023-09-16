39736c40393c414dfbdab7971704e03b37fccc04
ddanglehee <shee9812@naver.com>
2023-09-16 18:52:19 +0900

    [Set] BOJ - 10815번 숫자 카드 풀이

---
+++ boj/datastructure/set/Boj10815.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Boj10815 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n  = Integer.parseInt(br.readLine());
+        HashSet<Integer> nSet= new HashSet<>();
+
+        StringTokenizer st = new StringTokenizer();
+
+
+    }
+}
