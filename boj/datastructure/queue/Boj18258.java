3b9a15a9c7ea8fe2d68e0aeea0c5ed49a6b68340
ddanglehee <shee9812@naver.com>
2023-09-29 23:07:12 +0900

    [Queue] BOJ - 18258번 큐 2 풀이

--- boj/datastructure/queue/Boj18258.java
+++ boj/datastructure/queue/Boj18258.java
@@ -3,6 +3,52 @@ import java.util.*;
 
 public class Boj12852 {
 
+    // 정답 코드
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] visited = new int[n + 1];
+        ArrayDeque<Integer> queue = new ArrayDeque<>();
+        queue.add(n);
+
+        while (!queue.isEmpty()) {
+            Integer num = queue.removeFirst();
+
+            if (num == 1) {
+                break;
+            }
+
+            if (num % 3 == 0 && visited[num / 3] == 0) {
+                visited[num / 3] = num;
+                queue.add(num / 3);
+            }
+            if (num % 2 == 0 && visited[num / 2] == 0) {
+                visited[num / 2] = num;
+                queue.add(num / 2);
+            }
+            if (visited[num - 1] == 0) {
+                visited[num - 1] = num;
+                queue.add(num - 1);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        LinkedList<Integer> answer = new LinkedList<>();
+        int cur = 1;
+        while (cur != 0) {
+            answer.addFirst(cur);
+            cur = visited[cur];
+        }
+
+        sb.append(answer.size() - 1).append("\n");
+        for (int num: answer) {
+            sb.append(num).append(" ");
+        }
+        System.out.print(sb);
+    }
+
+    // 메모리 초과 코드
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
