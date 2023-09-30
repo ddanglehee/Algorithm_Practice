329a3f8cc12284dc18d27c25e4191c42ce0502c3
ddanglehee <shee9812@naver.com>
2023-09-30 11:22:30 +0900

    [String] BOJ - 20291번 파일 정리 최적화 풀이

--- boj/string/Boj20291.java
+++ boj/string/Boj20291.java
@@ -6,32 +6,20 @@ public class Boj20291 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        HashMap<String, Integer> map = new HashMap<>();
+        TreeMap<String, Integer> map = new TreeMap<>();
 
         for (int i = 0; i < n; i++) {
             String file = br.readLine();
-            for (int j = file.length() - 1; 0 <= j; j--) {
-                char c = file.charAt(j);
+            int j = file.indexOf('.');
 
-                if (c == '.') {
-                    String ext = file.substring(j + 1);
+            String ext = file.substring(j + 1);
 
-                    map.put(ext, map.getOrDefault(ext, 0) + 1);
-                    break;
-                }
-            }
+            map.put(ext, map.getOrDefault(ext, 0) + 1);
         }
-
-        String[] extArray = new String[map.keySet().size()];
-        int i = 0;
-        for (String ext: map.keySet()) {
-            extArray[i++] = ext;
-        }
-        Arrays.sort(extArray);
 
         StringBuilder sb = new StringBuilder();
-        for (String ext: extArray) {
-            sb.append(ext).append(" ").append(map.get(ext)).append("\n");
+        for (Map.Entry<String, Integer> entry: map.entrySet()) {
+            sb.append(entry.getKey()).append(" ").append(entry.getValue()).append("\n");
         }
 
         System.out.print(sb);
