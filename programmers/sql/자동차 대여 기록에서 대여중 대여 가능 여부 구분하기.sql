1c31dd9a9c3c5312d950e6ce613de975b22c3450
ddanglehee <shee9812@naver.com>
2023-10-16 22:42:59 +0900

    [SQL] 프로그래머스 - 자동차 대여 기록에서 대여중/대여 가능 여부 구분하기 풀이

---
+++ programmers/sql/자동차 대여 기록에서 대여중 대여 가능 여부 구분하기.sql
@@ -0,0 +1,8 @@
+select CAR_ID, if (CAR_ID in (
+    select CAR_ID
+    from CAR_RENTAL_COMPANY_RENTAL_HISTORY
+    where '2022-10-16' between START_DATE and END_DATE), '대여중', '대여 가능'
+    ) as AVAILABILITY
+from CAR_RENTAL_COMPANY_RENTAL_HISTORY
+group by CAR_ID
+order by CAR_ID desc
