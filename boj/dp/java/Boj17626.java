0187fa42c0497b1302551c23d5b03b51a4d34f2c
ddanglehee <shee9812@naver.com>
2025-02-10 11:37:42 +0900

    [Dynamic Programming] BOJ - 17626번 Four Squares더 효율적인  풀이

--- boj/dp/java/Boj17626.java
+++ boj/dp/java/Boj17626.java
@@ -1,6 +1,25 @@
 import java.io.*;
 import java.util.*;
 
+public class Boj17626 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp,4); // 모든 자연수는 넷 혹은 그 이하의 제곱수의 합으로 표현할 수 있다.
+
+        dp[0] = 0; dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1); // 1은 dp[j * j]
+            }
+        }
+
+        System.out.println(dp[n]);
+    }
+}
+
 public class Main {
 
     public static void main(String[] args) throws IOException {
@@ -28,3 +47,5 @@ public class Main {
         System.out.println(dp[n]);
     }
 }
+
+
