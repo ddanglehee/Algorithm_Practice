4803b01a9717c00ed470eb8fc77cb06b5a30592d
ddanglehee <shee9812@naver.com>
2025-09-26 14:01:19 +0900

    [SQL] 프로그래머스 - 식품분류별 가장 비싼 식품의 정보 조회하기 윈도우 함수로 풀이

--- programmers/sql/식품분류별 가장 비싼 식품의 정보 조회하기.sql
+++ programmers/sql/식품분류별 가장 비싼 식품의 정보 조회하기.sql
@@ -6,3 +6,15 @@ from FOOD_PRODUCT as A join (
     group by CATEGORY
 ) as B on A.CATEGORY = B.CATEGORY and A.PRICE = B.MAX_PRICE
 order by MAX_PRICE desc
+
+---
+
+select CATEGORY, PRICE as MAX_PRICE, PRODUCT_NAME
+from (
+         select CATEGORY, PRICE, PRODUCT_NAME,
+                rank() over(partition by CATEGORY order by PRICE desc) as rnk
+         from FOOD_PRODUCT
+         where CATEGORY in ('과자', '국', '김치', '식용유')
+     ) t
+where rnk = 1
+order by MAX_PRICE desc
