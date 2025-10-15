6fe98524e970fd8d80e6fae77b27db2c2499d2bb
ddanglehee <shee9812@naver.com>
2025-10-15 14:45:27 +0900

    [Dynamic Programming] BOJ - 11055번 가장 큰 증가하는 부분 수열 풀이

---
+++ boj/dp/java/Boj11055.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+class Boj11055 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] a = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[N];
+        for (int i = 0; i < N; i++) {
+            dp[i] = a[i];
+        }
+
+        for (int i = 1; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                if (a[j] < a[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + a[i]);
+                }
+            }
+        }
+
+        int answer = 0;
+        for (int i = 0; i < N; i++) {
+            answer = Math.max(answer, dp[i]);
+        }
+
+        System.out.print(answer);
+    }
+}
