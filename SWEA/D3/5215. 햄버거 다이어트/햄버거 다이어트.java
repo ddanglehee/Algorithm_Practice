5e51d2fb24d4ede0b4058a80f0f3030744f44420
장성희 <87361140+ddanglehee@users.noreply.github.com>
2026-02-06 16:33:25 +0900

    [D3] Title: 햄버거 다이어트, Time: 99 ms, Memory: 27,264 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -4,7 +4,8 @@ import java.io.*;
 public class Solution {
 	
 	static int N, L;
-	static List<Hamburger> burgerList = new ArrayList<>();
+	static int[] t = new int[1000];
+	static int[] k = new int[1000];
 	static int answer = 0;
 
 	public static void main(String[] args) throws IOException {
@@ -15,19 +16,16 @@ public class Solution {
 		StringBuilder sb = new StringBuilder();
 		for (int test_case = 1; test_case <= T; test_case++) {
 			answer = 0;
-			burgerList.clear();
+
 			st = new StringTokenizer(br.readLine());
 			
 			N = Integer.parseInt(st.nextToken());
 			L = Integer.parseInt(st.nextToken());
 			for (int i = 0; i < N; i++) {
 				st = new StringTokenizer(br.readLine());
-				int t = Integer.parseInt(st.nextToken());
-				int k = Integer.parseInt(st.nextToken());
-				burgerList.add(new Hamburger(t, k));
+				t[i] = Integer.parseInt(st.nextToken());
+				k[i] = Integer.parseInt(st.nextToken());
 			}
-			
-			Collections.sort(burgerList, (o1, o2) -> o1.kcal - o2.kcal);
 			solution(0, 0, 0);
 			
 			sb.append("#").append(test_case).append(" ").append(answer).append("\n");
@@ -36,24 +34,13 @@ public class Solution {
 	}
 	
 	static void solution(int index, int curKcal, int curScore) {
-		answer = Math.max(answer, curScore);
 		
-		if (index == N) return;
-		
-		Hamburger h = burgerList.get(index);
-		if (L < curKcal + h.kcal) return;
-		
-		solution(index + 1, curKcal + h.kcal, curScore + h.score);
-		solution(index + 1, curKcal, curScore);
-	}
-	
-	static class Hamburger {
-		int score;
-		int kcal;
-		
-		public Hamburger(int score, int kcal) {
-			this.score = score;
-			this.kcal =kcal;
+		if (index == N) {
+			if (curKcal <= L) answer = Math.max(answer, curScore);
+			return;
 		}
+		
+		solution(index + 1, curKcal + k[index], curScore + t[index]);
+		solution(index + 1, curKcal, curScore);
 	}
 }
