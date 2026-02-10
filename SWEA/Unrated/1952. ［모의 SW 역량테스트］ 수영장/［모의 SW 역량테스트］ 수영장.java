493a1bcb20c2be96ed492aee95692c2638731b82
장성희 <87361140+ddanglehee@users.noreply.github.com>
2026-02-11 08:27:08 +0900

    [Unrated] Title: [모의 SW 역량테스트] 수영장, Time: 85 ms, Memory: 26,112 KB -BaekjoonHub

--- SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
+++ SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
@@ -8,7 +8,7 @@ public class Solution {
     static StringBuilder sb = new StringBuilder();
 
     static int[] plans = new int[13];
-    static int[] memo = new int[13];
+    static int[] dp = new int[13]; // month월까지 처리했을 때 최소 누적 비용
     static int[] fee = new int[3]; // 이용권 가격 0: 1일, 1: 1달, 2: 3달
     static int answer;
 
@@ -24,40 +24,28 @@ public class Solution {
 
             // 1년 이용권을 answer의 초기값으로 세팅
             answer = Integer.parseInt(st.nextToken());
-            Arrays.fill(memo, answer);
 
             st = new StringTokenizer(br.readLine());
             for (int month = 1; month <= 12; month++) {
                 plans[month] = Integer.parseInt(st.nextToken());
             }
 
-            backtracking(1, 0);
+            for (int month = 1; month <= 12; month++) {
+                // 1일권과 1달권 중 저렴한 것으로 이번 달을 채움
+                dp[month] = dp[month - 1] + Math.min(plans[month] * fee[0], fee[1]);
+
+                // 3월 이상이라면, 2달 전에 3달권을 끊고 온 것과 비교
+                if (month >= 3) {
+                    dp[month] = Math.min(dp[month], dp[month - 3] + fee[2]);
+                }
+            }
+
+            // 최종적으로 계산된 12월까지의 합산과 1년권 중 최솟값 비교
+            answer = Math.min(answer, dp[12]);
 
             sb.append("#").append(t).append(" ").append(answer).append("\n");
         }
 
-        System.out.print(sb);
-    }
-
-    static void backtracking(int month, int cost) {
-
-        if (answer <= cost) return;
-
-        if (12 < month) {
-            answer = cost;
-            return;
-        }
-
-        if (memo[month] <= cost) return;
-        memo[month] = cost; // month월을 처리하기 직전까지의 최소 누적 비용
-
-        // 1일 이용권
-        backtracking(month + 1, cost + fee[0] * plans[month]);
-
-        // 1달 이용권
-        backtracking(month + 1, cost + fee[1]);
-
-        // 3달 이용권
-        backtracking(month + 3, cost + fee[2]);
+        System.out.println(sb);
     }
 }
