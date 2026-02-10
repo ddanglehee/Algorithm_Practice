838fd15f7646a112e41cf239b5c2c7381e3c2f54
장성희 <87361140+ddanglehee@users.noreply.github.com>
2026-02-10 23:52:49 +0900

    [Unrated] Title: [모의 SW 역량테스트] 수영장, Time: 78 ms, Memory: 26,240 KB -BaekjoonHub

--- SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
+++ SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
@@ -2,86 +2,62 @@ import java.util.*;
 import java.io.*;
 
 public class Solution {
-	
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer st;
-	static StringBuilder sb = new StringBuilder();
-	
-	static List<Plan> plans = new ArrayList<>();
-	static int[] prices = new int[3]; // 이용권 가격 0: 1일, 1: 1달, 2: 3달
-	static int answer;
-	
-	public static void main(String[] args) throws IOException {
-		int T = Integer.parseInt(br.readLine());
-		
-		
-		for (int t = 1; t <= T; t++) {
-			init();
-			
-			// 입력
-			st = new StringTokenizer(br.readLine());
-			for (int ticket = 0; ticket < 3; ticket++) {
-				prices[ticket] = Integer.parseInt(st.nextToken());
-			}
-			
-			// 1년 이용권을 answer의 초기값으로 세팅
-			answer = Integer.parseInt(st.nextToken());
-			
-			st = new StringTokenizer(br.readLine());
-			for (int month = 1; month <= 12; month++) {
-				int days = Integer.parseInt(st.nextToken());
-				
-				if (days == 0) continue;
-				plans.add(new Plan(month, days));
-			}
-			
-			if (plans.size() == 0) {
-				answer = 0;
-			} else {
-				backtracking(0, 0);
-			}
-			
-			sb.append("#").append(t).append(" ").append(answer).append("\n");
-		}
-		
-		System.out.print(sb);
-	}
-	
-	static void backtracking(int planIndex, int cost) {
-		
-		// 가지치기
-		if (answer <= cost) return;
-		
-		if (planIndex == plans.size()) {
-			answer = Math.min(answer, cost);
-			return;
-		}
-		
-		Plan plan = plans.get(planIndex);
-		
-		// 1일 이용권
-		backtracking(planIndex + 1, cost + prices[0] * plan.days);
-		
-		// 1달 이용권
-		backtracking(planIndex + 1, cost + prices[1]);
-		
-		// 3달 이용권
-		int nextIndex = planIndex + 1;
-		while (nextIndex < plans.size() && plans.get(nextIndex).month < plan.month + 3) nextIndex++;
-		backtracking(nextIndex, cost + prices[2]);
-	}
-	
-	static void init() {
-		plans.clear();
-	}
-	
-	static class Plan {
-		int month;
-		int days;
-		
-		Plan(int month, int days) {
-			this.month = month;
-			this.days = days;
-		}
-	}
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    static int[] plans = new int[13];
+    static int[] memo = new int[13];
+    static int[] fee = new int[3]; // 이용권 가격 0: 1일, 1: 1달, 2: 3달
+    static int answer;
+
+    public static void main(String[] args) throws IOException {
+        int T = Integer.parseInt(br.readLine());
+
+        for (int t = 1; t <= T; t++) {
+            // 입력
+            st = new StringTokenizer(br.readLine());
+            for (int ticket = 0; ticket < 3; ticket++) {
+                fee[ticket] = Integer.parseInt(st.nextToken());
+            }
+
+            // 1년 이용권을 answer의 초기값으로 세팅
+            answer = Integer.parseInt(st.nextToken());
+            Arrays.fill(memo, answer);
+
+            st = new StringTokenizer(br.readLine());
+            for (int month = 1; month <= 12; month++) {
+                plans[month] = Integer.parseInt(st.nextToken());
+            }
+
+            backtracking(1, 0);
+
+            sb.append("#").append(t).append(" ").append(answer).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+
+    static void backtracking(int month, int cost) {
+
+        if (answer <= cost) return;
+
+        if (12 < month) {
+            answer = cost;
+            return;
+        }
+
+        if (memo[month] <= cost) return;
+        memo[month] = cost; // month월을 처리하기 직전까지의 최소 누적 비용
+
+        // 1일 이용권
+        backtracking(month + 1, cost + fee[0] * plans[month]);
+
+        // 1달 이용권
+        backtracking(month + 1, cost + fee[1]);
+
+        // 3달 이용권
+        backtracking(month + 3, cost + fee[2]);
+    }
 }
