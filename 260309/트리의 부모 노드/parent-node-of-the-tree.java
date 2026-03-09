f9e16dcbecbae675487d7bf51b730e4eeaa6b396
장성희 <87361140+ddanglehee@users.noreply.github.com>
2026-03-09 10:27:30 +0900

    Title: 트리의 부모 노드, Time: 518ms, Memory: 37MB, Status: Passed - Codetree

--- 260309/트리의 부모 노드/parent-node-of-the-tree.java
+++ 260309/트리의 부모 노드/parent-node-of-the-tree.java
@@ -4,18 +4,45 @@ import java.io.*;
 public class Main {
 
     static int N;
+    static ArrayList<Integer>[] adjList;
+    static int[] parent;
+    static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine()) - 1;
+        N = Integer.parseInt(br.readLine());
 
-        StringBuilder sb = new StringBuilder();
+        adjList = new ArrayList[N+1];
+        parent = new int[N+1];
+        visited = new boolean[N+1];
+        for (int i = 1; i <= N; i++) {
+            adjList[i] = new ArrayList<>();
+        }
         StringTokenizer st;
-        while (N-- > 0) {
+        for (int i = 1; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            sb.append(st.nextToken()).append("\n");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            adjList[a].add(b);
+            adjList[b].add(a);
         }
 
+        travel(1);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 2; i <= N; i++) {
+            sb.append(parent[i]).append("\n");
+        }
         System.out.print(sb);
+    }
+
+    static void travel(int n) {
+        visited[n] = true;
+        for (int nxt : adjList[n]) {
+            if (visited[nxt]) continue;
+            parent[nxt] = n;
+            travel(nxt);
+        }
     }
 }
