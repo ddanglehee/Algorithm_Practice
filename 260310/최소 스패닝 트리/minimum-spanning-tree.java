b9177e7759d81a4175b2c85fc2fcce8e04745a6a
장성희 <87361140+ddanglehee@users.noreply.github.com>
2026-03-10 17:32:39 +0900

    Title: 최소 스패닝 트리, Time: 476ms, Memory: 83MB, Status: Runtime Error - Codetree

---
+++ 260310/최소 스패닝 트리/minimum-spanning-tree.java
@@ -0,0 +1,73 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int N, M;
+    static int answer;
+    static PriorityQueue<Edge> pq = new PriorityQueue<>();
+    static int[] parent;
+    static int count;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        parent = new int[N+1];
+        for (int i = 1; i <= N; i++) {
+            parent[i] = i;
+        }
+
+        while (M-- > 0) {
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            pq.offer(new Edge(a, b, c));
+        }
+
+        while (!pq.isEmpty()) {
+            Edge edge = pq.poll();
+
+            int aParent = find(edge.a);
+            int bParent = find(edge.b);
+
+            if(aParent == bParent) continue;
+            parent[aParent] = bParent;
+            answer += edge.c;
+            count++;
+            if (count == N - 1) break;
+        }
+
+        System.out.print(answer);
+    }
+
+    static int find(int x) {
+        if (parent[x] == x) {
+            return x;
+        }
+        return parent[x] = find(x);
+    } 
+
+    static class Edge implements Comparable<Edge> {
+        int a;
+        int b;
+        int c;
+
+        public Edge(int a, int b, int c) {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        @Override
+        public int compareTo(Edge o) {
+            return Integer.compare(this.c, o.c);
+        } 
+    }
+}
