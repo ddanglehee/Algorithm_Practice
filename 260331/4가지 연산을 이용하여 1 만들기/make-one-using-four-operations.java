862eec91c2ad0dcaed44fc34204def8cbf88cba8
장성희 <87361140+ddanglehee@users.noreply.github.com>
2026-03-31 17:20:48 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260331/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        System.out.print(bfs(N));
+    }
+
+    static int bfs(int N) {
+        Queue<int[]> queue = new ArrayDeque<>();
+        boolean[] visited = new boolean[2000000];
+        visited[N] = true;
+        queue.add(new int[] {N, 0});
+
+        ArrayList<Integer> list = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+
+            list.clear();
+
+            int count = cur[1] + 1;
+
+            list.add(cur[0] + 1);
+            list.add(cur[0] - 1);
+
+            if (cur[0] % 2 == 0) {
+                list.add(cur[0] / 2);
+            }
+            if (cur[0] % 3 == 0) {
+                list.add(cur[0] / 3);
+            }
+
+            for (int num : list) {
+                if (num == 1) return count;
+
+                if (!visited[num]) {
+                    visited[num] = true;
+                    queue.add(new int[] {num, count});
+                }
+            }
+        }
+        return -1;
+    }
+}
