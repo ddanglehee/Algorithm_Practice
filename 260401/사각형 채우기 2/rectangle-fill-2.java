c0c2592f10f8f38601cefc51c4b99361bae3cdf9
장성희 <87361140+ddanglehee@users.noreply.github.com>
2026-04-01 12:30:58 +0900

    Title: 사각형 채우기 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260401/사각형 채우기 2/rectangle-fill-2.java
+++ 260401/사각형 채우기 2/rectangle-fill-2.java
@@ -7,7 +7,7 @@ public class Main {
 
         dp[0] = dp[1] = 1;
         for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i-1] + dp[i - 2] * 2;
+            dp[i] = (dp[i-1] + dp[i - 2] * 2) % 10007;
         }
 
         System.out.print(dp[n]);
