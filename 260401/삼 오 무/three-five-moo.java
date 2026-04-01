462047dbc2e83005ab50426039f92d289c401cbf
장성희 <87361140+ddanglehee@users.noreply.github.com>
2026-04-01 17:00:46 +0900

    Title: 삼 오 무, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260401/삼 오 무/three-five-moo.java
+++ 260401/삼 오 무/three-five-moo.java
@@ -12,15 +12,14 @@ public class Main {
             long m = (s + e) / 2;
 
             // m 이 몇 번째로 적히는 숫자인가?
+            // m이 3의 배수거나 5의 배수일 수 있음
             long n = m - (m / 3 + m / 5 - m / 15);
 
-            if (n == N) {
+            if (N <= n) {
                 answer = m;
-                break;
+                e = m - 1;
             } else if (n < N) {
                 s = m + 1;
-            } else {
-                e = m - 1;
             }
         }
 
