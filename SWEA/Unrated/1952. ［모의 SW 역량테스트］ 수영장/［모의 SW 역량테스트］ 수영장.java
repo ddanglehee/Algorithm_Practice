6a8860a5efc4472b9f238ab66a5e510949cf9a5f
장성희 <87361140+ddanglehee@users.noreply.github.com>
2026-04-06 14:36:31 +0900

    [Unrated] Title: [모의 SW 역량테스트] 수영장, Time: 85 ms, Memory: 26,112 KB -BaekjoonHub

--- SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
+++ SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
@@ -2,50 +2,39 @@ import java.util.*;
 import java.io.*;
 
 public class Solution {
-
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-    static StringBuilder sb = new StringBuilder();
-
-    static int[] plans = new int[13];
-    static int[] dp = new int[13]; // month월까지 처리했을 때 최소 누적 비용
-    static int[] fee = new int[3]; // 이용권 가격 0: 1일, 1: 1달, 2: 3달
-    static int answer;
-
-    public static void main(String[] args) throws IOException {
-        int T = Integer.parseInt(br.readLine());
-
-        for (int t = 1; t <= T; t++) {
-            // 입력
-            st = new StringTokenizer(br.readLine());
-            for (int ticket = 0; ticket < 3; ticket++) {
-                fee[ticket] = Integer.parseInt(st.nextToken());
-            }
-
-            // 1년 이용권을 answer의 초기값으로 세팅
-            answer = Integer.parseInt(st.nextToken());
-
-            st = new StringTokenizer(br.readLine());
-            for (int month = 1; month <= 12; month++) {
-                plans[month] = Integer.parseInt(st.nextToken());
-            }
-
-            for (int month = 1; month <= 12; month++) {
-                // 1일권과 1달권 중 저렴한 것으로 이번 달을 채움
-                dp[month] = dp[month - 1] + Math.min(plans[month] * fee[0], fee[1]);
-
-                // 3월 이상이라면, 2달 전에 3달권을 끊고 온 것과 비교
-                if (month >= 3) {
-                    dp[month] = Math.min(dp[month], dp[month - 3] + fee[2]);
-                }
-            }
-
-            // 최종적으로 계산된 12월까지의 합산과 1년권 중 최솟값 비교
-            answer = Math.min(answer, dp[12]);
-
-            sb.append("#").append(t).append(" ").append(answer).append("\n");
-        }
-
-        System.out.println(sb);
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		
+		int[] ticket = new int[4];
+		int[] schedule = new int[13];
+		int[] dp = new int[13];
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		for (int t = 1; t <= T; t++) {
+			Arrays.fill(dp, 1_000_000_000);
+			
+			st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < 4; i++) {
+				ticket[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			st = new StringTokenizer(br.readLine());
+			for (int month = 1; month <= 12; month++) {
+				schedule[month] = Integer.parseInt(st.nextToken());
+			}
+			
+			dp[0] = 0;
+			dp[1] = Math.min(ticket[0] * schedule[1], ticket[1]);
+			dp[2] = dp[1] + Math.min(ticket[0] * schedule[2], ticket[1]);
+			dp[12] = ticket[3];
+			for (int i = 3; i <= 12; i++) {
+				dp[i] = Math.min(dp[i], Math.min(dp[i - 1] + Math.min(ticket[0] * schedule[i], ticket[1]), dp[i - 3] + ticket[2]));
+			}
+			
+			sb.append("#").append(t).append(" ").append(dp[12]).append("\n");
+		}
+		
+		System.out.print(sb);
+	}
 }
